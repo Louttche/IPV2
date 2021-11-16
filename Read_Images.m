de8bb9b0e217a3f7cdfab26ef663a17568c24c77
i375722 <i375722@fhict.nl>
2021-11-16 12:56:58 +0100

    Task 1 (+ some of 2)

--- Read_Images.m
+++ Read_Images.m
@@ -1,9 +1,12 @@
-function [images] = Read_Images(img_dir, pattern)
+function [images] = Read_Images(img_dir, pattern, dip)
 %READ_IMAGES Reads PNG images from specified directory
     
     % If pattern is not given set to default (By counting arguments given)
     if nargin < 2
         pattern = '*.png';
+        dip = false;
+    elseif nargin < 3
+        dip = false;
     end
     
     filePattern = fullfile(img_dir, pattern);
@@ -14,7 +17,11 @@ function [images] = Read_Images(img_dir, pattern)
     images = {0};
     for k = 1 : length(img_files)
         fullFileName = fullfile(img_files(k).folder, img_files(k).name);
-        images{k} = imread(fullFileName);
+        if (dip == false)
+            images{k} = imread(fullFileName);
+        else
+            images{k} = dip_image(fullFileName);
+        end
     end
     
     disp("Read " + length(img_files) + "/" + length(img_files));
